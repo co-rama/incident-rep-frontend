fac6557b5325e49d432e1d8ac26e603c827d6bfb
corama <coramayunus@gmail.com>
2024-10-13 17:43:54 +0300

    Style: Add a dynamic formgroup with items

--- src/app/pages/incidents/new-incident/new-incident.component.ts
+++ src/app/pages/incidents/new-incident/new-incident.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { MaterialImportModule } from '../../../shared/material-import.module';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-incident',
   standalone: true,
-  imports: [MaterialImportModule, FormsModule],
+  imports: [MaterialImportModule, FormsModule, CommonModule],
   templateUrl: './new-incident.component.html',
   styleUrl: './new-incident.component.scss',
 })
 export class NewIncidentComponent {
-submitNewIncident() {
-}
+  items = [{ title: '', quantity: 0, cost: 0, specification: '' }];
+
+  addItem() {
+    this.items.push({ title: '', quantity: 0, cost: 0, specification: '' });
+  }
+
+  removeItem(index: number) {
+    this.items.splice(index, 1);
+  }
+  submitNewIncident() {}
 }
