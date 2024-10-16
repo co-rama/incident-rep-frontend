d15f0ba8cdd8a524a3ca5ba1a32d32cfe2dcb020
corama <coramayunus@gmail.com>
2024-10-16 23:44:25 +0300

    Initial commit for searched component

---
+++ src/app/pages/incidents/searched-incidents/searched-incidents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searched-incidents',
+  standalone: true,
+  imports: [],
+  templateUrl: './searched-incidents.component.html',
+  styleUrl: './searched-incidents.component.scss'
+})
+export class SearchedIncidentsComponent {
+
+}
